ef3c8c4fa7f7a381b6a0d08f33480a87acfbea8f
sumyya <sumyyah21@gmail.com>
2021-06-23 20:01:07 +0500

    connected to firebase

--- app/src/main/java/com/samia/final_project/Register.java
+++ app/src/main/java/com/samia/final_project/Register.java
@@ -4,30 +4,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Register extends AppCompatActivity {
-    Button btnSubmit;
-    Button btn2;
+    Button btnregister;
+    Button btnsignin;
+    EditText edt1, edt2;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        btnSubmit = (Button) findViewById(R.id.btnSignIn2);
-        btnSubmit.setOnClickListener(new View.OnClickListener(){
+        edt1 = findViewById(R.id.edtEmail);
+        edt2 = findViewById(R.id.edtRegPassword);
+        btnregister = (Button) findViewById(R.id.btnSignIn2);
+        btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Register.this, SignIn.class);
                 startActivity(i);
             }
         });
-        btn2= (Button) findViewById(R.id.btnSignUp);
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btnsignin = (Button) findViewById(R.id.btnSignUp);
+        btnsignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Register.this, MainActivity.class);
-                startActivity(i);
+                createaccount();
+            }
+        });
+    }
+    void createaccount()
+    {
+        String Email;
+        String password;
+        Email = edt1.getText().toString().trim();
+        password = edt2.getText().toString();
+        mAuth.createUserWithEmailAndPassword(Email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Register.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/samia/final_project/SignIn.java
+++ app/src/main/java/com/samia/final_project/SignIn.java
@@ -7,23 +7,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
     Button btnSubmit;
+    EditText edt1,edt2;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         btnSubmit = (Button) findViewById(R.id.SIgn_in);
+        edt1 = findViewById(R.id.edtEmail2);
+        edt2 = findViewById(R.id.edtPassword2);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(SignIn.this, MainActivity.class);
-                startActivity(i);
+
+                       loginAccount();
             }
         });
-
+    }
+    void loginAccount()
+    {
+        String Email;
+        String password;
+        Email = edt1.getText().toString().trim();
+        password = edt2.getText().toString();
+        mAuth.signInWithEmailAndPassword(Email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(SignIn.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
